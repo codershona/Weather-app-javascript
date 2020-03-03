5a0cecc5c47079cc028999196790378e4b55cdef
codershona <falgunimst95@gmail.com>
2020-03-03 17:11:11 +0100

    data added but paid needed

--- public/script.js
+++ public/script.js
@@ -17,12 +17,23 @@ searchBox.addListener('places_changed', () => {
       longitude: longitude
     })
   }).then(res => res.json()).then(data => {
-    console.log(data)
-    // setWeatherData(data, place.formatted_address)
+    // console.log(data)
+     setWeatherData(data, place.formatted_address)
   })
 
 })
 
+const locationElement = document.querySelector('[data-location]')
+const statusElement = document.querySelector('[data-status]')
+const temparatureElement = document.querySelector('[data-temparature]')
+const precipitationElement = document.querySelector('[data-precipitation]')
+const windElement = document.querySelector('[data-wind]')
+
+
 function setWeatherData(data, place) {
-  
+  locationElement.textContent = place
+  statusElement.textContent = data.summary
+  temparatureElement.textContent = data.temparature
+  precipitationElement.textContent = `${data.precipProbability * 100}%`
+  windElement.textContent = data.windSpeed
 }
